85d2b7983190da588c1986e763f652af82d08869
nabetama <mao.nabeta@gmail.com>
2019-06-16 10:57:10 +0900

    add Action

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ const REIMBURSEMENT = "REIMBURSEMENT";
 const BONUS = "BONUS";
 const STOCK_OPTIONS = "STOCK_OPTIONS";
 const PAY_DAY = "PAY_DAY";
+
+interface PayrollAction {
+  type: string;
+  amount?: number;
+}
